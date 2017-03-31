d014059182cc798e1194d369dbc4c92dfe1032bc
rookjc <rookjc@miamioh.edu>
2017-03-31 15:56:19 -0400

    Created basic features for Slot

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,6 +1,21 @@
 // Represents the current state of the game board, with 42 Pieces and 69 4-in-a-row Slots
 public class GameBoard {
 	private Piece[][] board;
+	private Slot[] slots;
+	
+	// Constructor for creating an empty GameBoard, along with all its component Pieces
+	public GameBoard () {
+		// Create grid of empty Pieces
+		this.board = new Piece[6][7];
+		for (int row = 0; row < 6; row++) {
+			for (int col = 0; col < 7; col++) {
+				board[row][col] = new Piece(row, col);
+			}
+		}
+		
+		this.slots = new Slot[69];
+		// TODO: Generate all the slots
+	}
 	
 	// Revert this GameBoard back to an empty grid
 	public void clearBoard() {
@@ -12,13 +27,9 @@ public class GameBoard {
 		}
 	}
 	
-	// Constructor for creating an empty GameBoard, along with all its component Pieces
-	public GameBoard () {
-		this.board = new Piece[6][7];
-		for (int row = 0; row < 6; row++) {
-			for (int col = 0; col < 7; col++) {
-				board[row][col] = new Piece(row, col);
-			}
-		}
+	// Get the Piece located at a particular row and column
+	public Piece getPiece(int row, int col) {
+		return board[row][col];
 	}
+	
 }
--- src/Slot.java
+++ src/Slot.java
@@ -1,4 +1,52 @@
 // Stub class to be implemented
 public class Slot {
-
+	// Constants for which of the 4 directions a slot can be oriented in
+	public static final int HORIZONTAL = 0;
+	public static final int VERTICAL = 1;
+	public static final int DIAGONAL_DOWN = 2;
+	public static final int DIAGONAL_UP = 3;
+	
+	public final int orientation;
+	private Piece[] pieces;
+	
+	// Generate a slot with the four pieces specified by an origin and an orientation
+	public Slot(GameBoard board, Piece origin, int orientation) {
+		this.orientation = orientation;
+		
+		// Figure out dx and dy, the relative positions of consecutive pieces
+		// in the slot, using the specified orientation
+		int dx, dy;
+		switch (orientation) {
+		case Slot.HORIZONTAL:
+			dx = 1; dy = 0;
+			break;
+		case Slot.VERTICAL:
+			dx = 0; dy = 1;
+			break;
+		case Slot.DIAGONAL_UP:
+			dx = 1; dy = 1;
+			break;
+		case Slot.DIAGONAL_DOWN:
+		default:
+			dx = 1; dy = -1;
+			break;
+		}
+		
+		// Extract the four needed pieces from the GameBoard
+		this.pieces = getPieces(board, origin, dx, dy);
+	}
+	
+	// Extract the four needed pieces based on origin and the separations dx and dy
+	public static Piece[] getPieces(GameBoard board, Piece origin, int dx, int dy) {
+		// Get coordinates (row & column) of origin
+		int originX = origin.col;
+		int originY = origin.row;
+		
+		// Iterate 4 times to get the four pieces, each differing in position by (dx,dy)
+		Piece[] result = new Piece[4];
+		for (int i = 0; i < 4; i++)
+			result[i] = board.getPiece(originY + i * dy, originX + i * dx);
+		
+		return result;
+	}
 }
