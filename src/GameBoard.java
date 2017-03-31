3a9d88e42ff9b28b28f3ead6da0dc8dd46afccf5
rookjc <rookjc@miamioh.edu>
2017-03-31 14:52:43 -0400

    Revert "Added basic features to GameBoard and Piece"
    This reverts commit 6d52a4d4025dc25842a0ce7a7060e8a2c2fb6fd5.

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,24 +1,4 @@
-// Represents the current state of the game board, with 42 Pieces and 69 4-in-a-row Slots
+// Stub class to be implemented
 public class GameBoard {
-	private Piece[][] board;
-	
-	// Revert this GameBoard back to an empty grid
-	public void clearBoard() {
-		// Set each of the 42 pieces to empty
-		for (int row = 0; row < 6; row++) {
-			for (int col = 0; col < 7; col++) {
-				board[row][col].setColor(Piece.EMPTY);
-			}
-		}
-	}
-	
-	// Constructor for creating an empty GameBoard, along with all its component Pieces
-	public GameBoard () {
-		this.board = new Piece[6][7];
-		for (int row = 0; row < 6; row++) {
-			for (int col = 0; col < 7; col++) {
-				board[row][col] = new Piece(row, col);
-			}
-		}
-	}
+
 }
