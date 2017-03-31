cf6108392398763137893387aaab8f555cab0c62
Kai Li <Kai Li>
2017-03-31 14:28:16 -0400

    GameApplicationInitializer

---
+++ src/Starter.java
@@ -0,0 +1,17 @@
+import javax.swing.SwingUtilities;
+
+public class Starter {
+
+	public static void main(String[] args) {
+		// running the graphic processing in a separate thread 
+				SwingUtilities.invokeLater(new Runnable() {
+
+					@Override
+					public void run() {
+						UserInterface uf = new UserInterface(); 
+						System.out.println("Current Thread: " + Thread.currentThread());
+					}
+				});
+	}
+
+}
