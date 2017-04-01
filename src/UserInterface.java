b9a2e4f7733e9b0501eeb646975136028bdb3ad4
Kai Li <Kai Li>
2017-03-31 23:00:11 -0400

    updated

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,15 +1,23 @@
 import java.awt.Color;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class UserInterface extends JFrame{
 	
+	private InterfacePanel interfacePanel; 
+	
 	public UserInterface() {
 		super("ConnectFour");
+		interfacePanel = new InterfacePanel(800,800); 
+		add(interfacePanel);
+		// frame set up 
+		
 		setSize(800, 800);
 		setVisible(true);
 		setResizable(false);
-		getContentPane().setBackground(new Color(118, 142,239));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
+	
+	
 }
