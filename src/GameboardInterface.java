ac3904bc45947392c9b60401cee7b460fa3ee95e
FinalClipX <joelminton64@gmail.com>
2017-04-19 13:44:26 -0400

    Board is now drawn; Resolution changed to 800x700

--- src/GameboardInterface.java
+++ src/GameboardInterface.java
@@ -3,6 +3,10 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.geom.Ellipse2D;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
@@ -12,7 +16,7 @@ public class GameboardInterface extends JPanel {
 	private ToolBar toolBar;
 
 	private JButton tempBtn;
-	
+
 	private GameBoard board;
 
 	public GameboardInterface() {
@@ -23,45 +27,58 @@ public class GameboardInterface extends JPanel {
 		toolBar.getQuitButton().setText("Quit");
 
 		add(toolBar, BorderLayout.NORTH);
-		
+
 		board = new GameBoard();	// Create the underlying data structure
-
-//		 Just a temporary button for
-//		 testing the statistic page
-//		 this button will be replaced
-//		 by GameBoard Page Panel
-//		tempBtn = new JButton("Show Stats");
-//		add(tempBtn);
-
+		
 		Color bg = new Color(118, 142, 239);
+
+//		for(int i = 0; i < 7; i++) {
+//			JButton column = new JButton();
+//			column.setPreferredSize(new Dimension(105, 670));
+//
+//			column.setBackground(Color.PINK);
+//
+//			final int col = i;
+//			column.addActionListener(new ActionListener() {
+//				@Override
+//				public void actionPerformed(ActionEvent e) {
+//					board.playerClick(col);
+//				}
+//			});
+//
+//			this.add(column);
+//		}
+	}
+
+	@Override
+	public void paintComponent(Graphics g)
+	{
+		int xOffset = 0;
+		int yOffset = 0;
 		
-		setBackground(bg);
-		
-		JPanel panel = new JPanel();
-		panel.setBackground(bg);
-		add(panel, BorderLayout.CENTER);
+		super.paintComponent(g);
 		
 		for(int i = 0; i < 7; i++) {
-			JButton column = new JButton();
-			column.setPreferredSize(new Dimension(105, 670));
+			for(int j = 0; j < 6; j++) {
+				
+				Graphics2D g2 = (Graphics2D)g;
+				g2.setColor(Color.WHITE);
+				Ellipse2D.Double circle = new Ellipse2D.Double(((i+1)*25 + xOffset), (100 + yOffset), 75, 75);
+				g2.fill(circle);
+				
+				yOffset += 93;
+			}
 			
-<<<<<<< HEAD
-			column.setBackground(bg);
-			
-=======
-			final int col = i;
->>>>>>> origin/master
-			column.addActionListener(new ActionListener() {
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					board.playerClick(col);
-				}
-			});
-			
-			panel.add(column);
-		}
+			xOffset += 84;
+			yOffset = 0;
+		}		
 	}
-	
+
+	@Override
+	public Dimension getPreferredSize() {
+		return new Dimension(300, 300); // appropriate constants
+	}
+
 	public JButton getTempBtn() {
 		return tempBtn;
 	}
