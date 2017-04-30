d66ea506f3c2ab397205d2ff9dbb3f34fc441dda
rookjc <rookjc@miamioh.edu>
2017-04-30 16:36:08 -0400

    Refined GameBoard.java comments

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,13 +1,18 @@
 import java.awt.Color;
 
-// Represents the current state of the game board, with 42 Pieces and 69 4-in-a-row Groups
-// TODO: make this inherit from Panel
+/**
+ * Represents the current state of the game board, with 42 Pieces and 69 4-in-a-row Groups
+ * @author Hayden Fogle, Kai Li, Joel Minton, Jayson Rook
+ * @version 1.0
+ */
 public class GameBoard {
 	private Piece[][] board;
 	private Groups[] groups;
 	private ComputerPlayer computer;
 	
-	// Constructor for creating an empty GameBoard, along with all its component Pieces
+	/**
+	 * Constructor for creating an empty GameBoard, along with all its component Pieces
+	 */
 	public GameBoard () {
 		// Create grid of empty Pieces
 		this.board = new Piece[6][7];
@@ -17,9 +22,9 @@ public class GameBoard {
 			}
 		}
 		
+		// Generate all possible groups into this.groups array
 		this.groups = new Groups[69];
 		int groupNum = 0;
-		// Generate all possible groups into this.groups array
 		for (int row = 0; row < 6; row++) {		// Horizontal
 			for (int col = 0; col < 4; col++)
 				this.groups[groupNum++] = new Groups(this, this.getPiece(row, col), Groups.HORIZONTAL);
@@ -41,7 +46,9 @@ public class GameBoard {
 		this.computer = new ComputerPlayer(this);
 	}
 	
-	// Revert this GameBoard back to an empty grid
+	/**
+	 * Revert this GameBoard back to an empty grid
+	 */
 	public void clearBoard() {
 		// Set each of the 42 pieces to empty
 		for (int row = 0; row < 6; row++) {
@@ -52,17 +59,22 @@ public class GameBoard {
 		GameState.playerTurn = true;
 	}
 	
-	// Get the Piece located at a particular row and column
+	/**
+	 * Get the Piece object at a particular position
+	 * @param row the row index (0-5)
+	 * @param col the column index (0-6)
+	 * @return the Piece object representing this position on the board
+	 */
 	public Piece getPiece(int row, int col) {
 		return board[row][col];
 	}
 	
-	// Get the Piece located right below the Piece at a particular row and column (or null if none exists)
-	public Piece getPieceBelow(int row, int col) {
-		return row == 0 ? null : board[row-1][col];
-	}
-	
-	// Place a piece of a given pieceColor into the column indexed by col
+	/**
+	 * Place a piece of a specified color into a specified column
+	 * @param col the column index (0-6)
+	 * @param pieceColor the color of the piece to be placed
+	 * @return true iff the move was valid
+	 */
 	public boolean makeMove(int col, Color pieceColor) {
 		int row = getColumnHeight(col);
 		if (row > 5)
@@ -72,7 +84,11 @@ public class GameBoard {
 		return true;
 	}
 	
-	// Gives the height of the stack of pieces in column number col
+	/**
+	 * Gives the height of the stack of pieces in the specified column
+	 * @param col the column index (0-6)
+	 * @return the height of the column (0 begin empty, 6 being full)
+	 */
 	public int getColumnHeight(int col) {
 		for (int row = 0; row < 6; row++) {
 			if (board[row][col].isEmpty())
@@ -83,22 +99,34 @@ public class GameBoard {
 	
 	// Get the number of pieces that must be placed in a piece's column,
 	// before that piece itself can be a valid move
+	/**
+	 * Get the number of pieces the must be placed in a column,
+	 * until the specified piece's location can be a valid move
+	 * @param p the Piece in question
+	 * @return this 'buildup' value (0-5) for the Piece p
+	 */
 	public int getBuildUp(Piece p) {
 		return p.row - getColumnHeight(p.col);
 	}
 	
-	// Check if either player has won, or the board is full, giving the proper GameState constant back
+	/**
+	 * Check if either player has won, or if the board is full
+	 * @return a GameState constant representing a win, loss, draw, or none of these
+	 */
 	public int checkGameOver() {
 		// Check all potential groups for wins
 		for (Groups s : groups) {
 			Color winner = s.getWinningColor();
 			if (winner.equals(Piece.RED)) {
-				s.setAllColors(Piece.RED_WIN);	// Show where the win is
+				// Show where the win is
+				s.setAllColors(Piece.RED_WIN);
+				// Update the win/loss count and return the state
 				GameState.addRedWin();
 				return GameState.playerIsRed ? GameState.PLAYERWON : GameState.PLAYERLOST;
 			} else if (winner.equals(Piece.YELLOW)) {
-				s.setAllColors(Piece.YELLOW_WIN);	// Show where the win is
+				// Show where the win is
 				GameState.addYellowWin();
+				// Update the win/loss count and return the state
 				return GameState.playerIsRed ? GameState.PLAYERLOST : GameState.PLAYERWON;
 			}
 		}
@@ -113,9 +141,12 @@ public class GameBoard {
 		return GameState.DRAW;
 	}
 
-	// Process an attempted move from the player. Return a GameState constant indicating win/loss/draw
+	/**
+	 * Process an attempted move from the player
+	 * @param col the column the player attempts to move in
+	 * @return a GameState constant indicating a win, loss, draw, invalid move, or none of these
+	 */
 	public int playerClick(int col) {
-		System.out.println("player moved in col: " + col);	// temporary
 		if (GameState.playerTurn) {
 			GameState.playerTurn = false;
 			if (makeMove(col, GameState.playerIsRed ? Piece.RED : Piece.YELLOW)) {
@@ -129,15 +160,21 @@ public class GameBoard {
 				state = checkGameOver();
 				if (state != GameState.NORMAL)
 					return state;
+				
 			} else {
 				return GameState.INVALIDMOVE;
 			}
 		}
+		
+		// Nothing special happened, so go back to the player's turn
 		GameState.playerTurn = true;
 		return GameState.NORMAL;
 	}
 	
-	// Getter for the slot array
+	/**
+	 * Getter for the array of groups
+	 * @return the array of all 69 groups on the board
+	 */
 	public Groups[] getGroups() {
 		return this.groups;
 	}
