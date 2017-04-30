726debdd74510af2100103e91a47e4b9aec5c6bd
rookjc <rookjc@miamioh.edu>
2017-04-30 17:10:27 -0400

    Piece.java javadoc comments

--- src/Piece.java
+++ src/Piece.java
@@ -1,6 +1,10 @@
 import java.awt.Color;
 
-// Represents one piece in the 6 by 7 GameBoard.board
+/**
+ * Represents one Piece (with color and coordinate position) in a 6 by 7 GameBoard
+ * @author Kai Li, Jayson Rook, Hayden Fogle, Joel Minton 
+ * @version 1.0
+ */
 public class Piece {
 	public static final Color EMPTY = Color.WHITE;
 	public static final Color RED = new Color(253, 105, 105);
@@ -14,32 +18,53 @@ public class Piece {
 	// Whether this is a red piece, yellow piece, or no piece (Piece.EMPTY)
 	private Color color;
 
-	// Initialize an empty piece
+	/**
+	 * Initialize an empty Piece
+	 * @param row the row index (0-5)
+	 * @param col the column index (0-6)
+	 */
 	public Piece(int row, int col) {
 		this.row = row;
 		this.col = col;
 		this.color = Piece.EMPTY;
 	}
-
+	
+	/**
+	 * Assign a new color to this Piece
+	 * @param color the new color value
+	 */
 	public void setColor(Color color) {
 		this.color = color;
 	}
 
+	/**
+	 * Get this Piece's color
+	 * @return the current color value
+	 */
 	public Color getColor() {
 		return this.color;
 	}
 
-	// True iff there is no piece here
+	/**
+	 * Determine whether or not a player's Piece is here
+	 * @return true iff there is no colored Piece here
+	 */
 	public boolean isEmpty() {
 		return this.color.equals(Piece.EMPTY);
 	}
 
-	// True iff the human's piece is here
+	/**
+	 * Determine whether or not the human's Piece is here
+	 * @return true iff the human's Piece is here
+	 */
 	public boolean isHuman() {
 		return this.color.equals(GameState.playerIsRed ? Piece.RED : Piece.YELLOW);
 	}
 
-	// True iff the computer's piece is here
+	/**
+	 * Determine whether or not the computer's Piece is here
+	 * @return true iff the computer's Piece is here
+	 */
 	public boolean isComputer() {
 		return this.color.equals(GameState.playerIsRed ? Piece.YELLOW : Piece.RED);
 	}
